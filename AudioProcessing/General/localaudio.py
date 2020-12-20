0cc1a9f352ff0eb4a1523338e1cf77722f81f76f
inafi <irfan.k.nafi@gmail.com>
2020-12-20 14:15:25 -0500

    arduino

--- AudioProcessing/General/localaudio.py
+++ AudioProcessing/General/localaudio.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 import math
+import serial
 
+s = serial.Serial(port='/dev/tty.usbserial-14230', baudrate=9600)
 mic = pyaudio.PyAudio()
 
 FORMAT = pyaudio.paInt16
@@ -16,7 +18,7 @@ stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE,
                   input=True, frames_per_buffer=CHUNK)
 
 def sig(num):
-    return 1/(1 + math.exp(-5 * num))
+    return 1/(1 + math.exp(-30 * num))
 
 def get_rms(block):
     count = len(block)/2
@@ -28,17 +30,19 @@ def get_rms(block):
         n = sample * (1.0/32768.0)
         sum_squares += n**2
         
-    return 100 * (sum_squares/count) ** 0.5
-    # return 2 * (100 * sig((sum_squares/count) ** 0.5) - 50), 100 * (sum_squares/count) ** 0.5
+    # return 100 * (sum_squares/count) ** 0.5
+    return int(2 * (100 * sig((sum_squares/count) ** 0.5) - 50))
 
 while True:
     data = stream.read(CHUNK, exception_on_overflow=False)
     # print(get_rms(data))
-    print(data)
-    print ("|" * int(get_rms(data) * 10))
+    # print(data)
+    amp = str(get_rms(data))
+    out = "<%s, %s, %s, %s, %s, %s>" % (amp, amp, amp, amp, amp, amp)
+    s.write(out.encode())
     data = np.frombuffer(data, dtype='b')
     volume_norm = int(np.linalg.norm(data)/100)
-    # print ("|" * int(volume_norm))
+    print ("|" * int(amp))
     # print(volume_norm)
 
 stream.stop_stream()
