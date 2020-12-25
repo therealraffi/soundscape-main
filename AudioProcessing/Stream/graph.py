bc2ec4b487ef9e2eaf6be93d84a3fe8c53aa274c
inafi <irfan.k.nafi@gmail.com>
2020-12-24 20:10:16 -0500

    threads

--- AudioProcessing/Stream/graph.py
+++ AudioProcessing/Stream/graph.py
@@ -9,6 +9,7 @@ import re
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import threading 
 
 cred = credentials.Certificate('soundy-8d98a-firebase-adminsdk-o03jf-c7fede8ea2.json')
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -56,52 +57,72 @@ def firejson(plot, colors):
     
     return "{" + out[:-2] + "}"
 
-while True:
-    data = s.recv(8192)
-    coord = data.decode().replace("\n", "")
-    try:
-        result = re.search(r'\[(.*?)]', coord).group(1)
-        xc = re.findall(r'"x": (.*?),', result)
-        yc = re.findall(r'"y": (.*?),', result)
-        zc = re.findall(r'"z": (.*?),', result)
-        points = []
-        ind = []
-        colors = ["r1", "g2", "b3", "m4"]
+data = ""
 
-        for i in range(len(xc)):
-            x = float(xc[i])
-            y = float(yc[i])
-            z = float(zc[i])
-            if x != 0 and y != 0 and z != 0:
-                theta = math.atan(y/x)
-                x = abs(math.cos(theta)) * x / abs(x) * -1
-                y = abs(math.sin(theta)) * y / abs(y)
-                points.append([x, y])
-                ind.append(colors[i])
+def getdata():
+    global data
+    while True:
+        data = s.recv(8192)
 
-        points = np.array(points)
-        
-        plot = []
-        for i in range(len(points)):
-            angle = math.atan2(points[i][1], points[i][0]) * 180 / math.pi + 60
-            if angle < 0:
-                angle += 360
-            angle *= math.pi/180
-            plot.append([math.cos(angle), math.sin(angle)])
+def listen():
+    global data
+    while True:
+        try:
+            coord = data.decode().replace("\n", "")
+            result = re.search(r'\[(.*?)]', coord).group(1)
+            xc = re.findall(r'"x": (.*?),', result)
+            yc = re.findall(r'"y": (.*?),', result)
+            zc = re.findall(r'"z": (.*?),', result)
+            points = []
+            ind = []
+            colors = ["r1", "g2", "b3", "m4"]
+
+            for i in range(len(xc)):
+                x = float(xc[i])
+                y = float(yc[i])
+                z = float(zc[i])
+                if x != 0 and y != 0 and z != 0:
+                    theta = math.atan(y/x)
+                    x = abs(math.cos(theta)) * x / abs(x) * -1
+                    y = abs(math.sin(theta)) * y / abs(y)
+                    points.append([x, y])
+                    ind.append(colors[i])
+
+            points = np.array(points)
             
-        fire = eval(firejson(plot, ind))
-        ref.set(fire)
+            plot = []
+            for i in range(len(points)):
+                angle = math.atan2(points[i][1], points[i][0]) * 180 / math.pi + 60
+                if angle < 0:
+                    angle += 360
+                angle *= math.pi/180
+                plot.append([math.cos(angle), math.sin(angle)])
+                
+            fire = eval(firejson(plot, ind))
+            print(fire)
+            ref.set(fire)
 
-        # print("wew")
-        #graph
-        # plot = np.array(plot)
-        # plot = plt.scatter(plot[:, 0], plot[:, 1], c=ind, s = 50)
-        # plt.pause(0.00001)
-        # plot.remove()
+            # print("wew")
+            #graph
+            # plot = np.array(plot)
+            # plot = plt.scatter(plot[:, 0], plot[:, 1], c=ind, s = 50)
+            # plt.pause(0.00001)
+            # plot.remove()
 
-    except KeyboardInterrupt as e:
-        break
-    except Exception as e:
-        print(e)
+        except KeyboardInterrupt as e:
+            break
+        except Exception as e:
+            print(e)
 
 # plt.show()
+
+if __name__ == "__main__": 
+    t1 = threading.Thread(target=getdata) 
+    t2 = threading.Thread(target=listen) 
+
+    t1.start() 
+    t2.start() 
+  
+    t1.join() 
+    t2.join() 
+  
