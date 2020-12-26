a2a385027c0c92f4bed36c06bfb1e1b95de8ac3c
inafi <irfan.k.nafi@gmail.com>
2020-12-26 12:54:18 -0500

    main

--- AudioProcessing/Stream/main.py
+++ AudioProcessing/Stream/main.py
@@ -21,8 +21,9 @@ firebase_admin.initialize_app(cred, {
 })
 
 #Global Data Vars
-graphdata = ""
-postdata = ""
+graphdata = b""
+postdata = b""
+angles = []
 
 def sep():
     while True:
@@ -106,11 +107,50 @@ def post():
     accept_connections()
 
 def position():
+    # while True:
+    #     try:
+    #         port = 9000
+    #         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #         s.bind((ip, port))
+    #         break
+    #     except:
+    #         print("Couldn't bind to that port")
+
+    # def accept_connections():
+    #     s.listen(100)
+
+    #     print('Running on IP: ' + ip)
+    #     print('Running on port: '+ str(port))
+        
+    #     while True:
+    #         c, addr = s.accept()
+    #         connections.append(c)
+    #         threading.Thread(target=handle_client,args=(c,addr,)).start()
+        
+    # def broadcast(sock, data):
+    #     for client in connections:
+    #         if client != s and client != sock:
+    #             try:
+    #                 client.send(data)
+    #             except:
+    #                 pass
+
+    # def handle_client(c, addr):
+    #     global graphdata
+    #     while True:
+    #         try:
+    #             graphdata = c.recv(8192)
+    #             broadcast(c, graphdata)
+    #         except socket.error:
+    #             c.close()
+
+    # connections = []
+    # accept_connections()
     global graphdata
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     while True:
         try:
-            target_ip = "192.168.1.218"
+            target_ip = "173.66.155.183"
             target_port = 9000
             s.connect((target_ip, target_port))
 
@@ -141,6 +181,8 @@ def firejson(plot, colors):
 
 def graph():
     global graphdata
+    global angles
+
     ref = db.reference('x_and_y')   
     while True:
         try:
@@ -165,23 +207,34 @@ def graph():
                     ind.append(colors[i])
 
             points = np.array(points)
-            
             plot = []
+            temp = []
+            
             for i in range(len(points)):
                 angle = math.atan2(points[i][1], points[i][0]) * 180 / math.pi + 60
                 if angle < 0:
                     angle += 360
+                temp.append([angle, int(ind[i][1])])
                 angle *= math.pi/180
                 plot.append([math.cos(angle), math.sin(angle)])
-                
+            
+            angles = temp
             fire = eval(firejson(plot, ind))
             ref.set(fire)
-
+        except KeyboardInterrupt as e:
+            pass
+        except Exception as e:
+            pass
+            
 def sig(num):
     return 1/(1 + math.exp(-10 * num))
 
 def amplitude(block):
     count = len(block)/2
+
+    if count == 0:
+        return None
+
     count = count if count != 0 else 1
     format = "%dh"%(count)
     shorts = struct.unpack(format, block)
@@ -195,7 +248,11 @@ def amplitude(block):
     return int(2 * (100 * sig((sum_squares/count) ** 0.5) - 50))
 
 def avgfreq(block):
-        result = np.fromstring(block, dtype=np.int16)
+        result = np.frombuffer(block, dtype=np.int16)
+
+        if len(result) == 0:
+            return None
+
         w = np.fft.fft(result)
         freqs = np.abs((np.fft.fftfreq(len(w))*44100))
         w = np.abs(w)
@@ -227,8 +284,71 @@ def avgfreq(block):
 
         return None if math.isnan(float(out)) or len(freqdict) == 0 else max(1, int(out))
 
-def ardunio():
-    pass
+#print("%3s %6s \t %3s %6s \t %3s %6s \t %3s %6s" % (amplitude(f0[-1]), avgfreq(f0[-1]), amplitude(f1[-1]), avgfreq(f1[-1]), amplitude(f2[-1]), avgfreq(f2[-1]), amplitude(f3[-1]), avgfreq(f3[-1])), amp * "|")
+
+def arduino():
+    global postdata
+    global angles
+    s = serial.Serial(port='/dev/tty.usbserial-14230', baudrate=9600)
+
+    while True:
+        channels = np.frombuffer(postdata, dtype='int16')
+        c0 = channels[0::8].tobytes() #red
+        c1 = channels[1::8].tobytes() #green
+        c2 = channels[2::8].tobytes() #blue
+        c3 = channels[3::8].tobytes() #purple
+
+        analysis = [[amplitude(c0), avgfreq(c0)], [amplitude(c1), avgfreq(c1)], [amplitude(c2), avgfreq(c2)], [amplitude(c3), avgfreq(c3)]]
+
+        ignore = 120
+
+        for c in range(len(angles) - 1, -1, -1):
+            i = angles[c]
+            if (180 - ignore)/2 < i[0] < (180 + ignore)/2:
+                del angles[i]
+
+        amps = [0] * 6
+        inc = (360 - ignore) / 5
+
+        possible = [(180 + ignore)/2 + i * inc for i in range(5)]
+        possible.insert(0, (180 - ignore)/2)
+
+        for i, color in angles:
+            ind = 0
+            for c, k in enumerate(possible):
+                if k - inc/2 <= i <= k + inc/2:
+                    ind = c
+                    if amps[ind] != 0:
+                        if ind == 0:
+                            if amps[1] == 0:
+                                ind = 1
+                                break
+                            if amps[5] == 0:
+                                ind = 5
+                                break
+                        elif ind == 5:
+                            if amps[0] == 0:
+                                ind = 0
+                                break
+                            if amps[4] == 0:
+                                ind = 4
+                                break
+                        else:
+                            if amps[ind + 1] == 0:
+                                ind += 1
+                                break
+                            if amps[ind - 1] == 0:
+                                ind -= 1
+                                break
+            amps[ind] = max(analysis[color][0], 1)
+
+        print(angles)
+        print(analysis)
+        print(amps)
+        print("\n\n")
+
+        # out = "<%s, %s, %s, %s, %s, %s>" % (amps[0], amp[1], amp[2], amp[3], amp[4], amp[5])
+        # s.write(out.encode())
 
 if __name__ == "__main__": 
     t1 = threading.Thread(target=sep) 
