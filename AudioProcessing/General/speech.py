f76faadbc5900e8dd1f620e49a9e861fc7f635e6
inafi <irfan.k.nafi@gmail.com>
2020-12-25 21:25:10 -0500

    finale

--- AudioProcessing/General/speech.py
+++ AudioProcessing/General/speech.py
@@ -27,6 +27,11 @@ CHUNK_SIZE = 8192  # 100ms
 #Streaming Client
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+#Global Vars
+fm = []
+f0 = []
+finalspeech = ""
+
 while True:
     try:
         target_ip = "173.66.155.183"
@@ -39,64 +44,57 @@ while True:
 def get_current_time():
     return int(round(time.time() * 1000))
 
-fm = []
-f0 = []
-mic = []
-
 class ResumableMicrophoneStream:
     def __init__(self, rate, chunk_size):
-        self._rate = rate
-        self.chunk_size = chunk_size
-        self._num_channels = 1
-        self._buff = queue.Queue()
-        self.closed = True
-        self.start_time = get_current_time()
-        self.restart_counter = 0
-        self.audio_input = []
-        self.last_audio_input = []
-        self.result_end_time = 0
-        self.is_final_end_time = 0
-        self.final_request_end_time = 0
-        self.bridging_offset = 0
-        self.last_transcript_was_final = False
-        self.new_stream = True
-        self._audio_interface = pyaudio.PyAudio()
-        # self._audio_stream = self.sound
-        self._audio_stream = self._audio_interface.open(
-            format=pyaudio.paInt16,
-            channels=self._num_channels,
-            rate=self._rate,
-            input_device_index=3,
-            input=True,
-            frames_per_buffer=self.chunk_size,
-            stream_callback=self._fill_buffer
-        )
+        def init():
+            self._rate = rate
+            self.chunk_size = chunk_size
+            self._num_channels = 1
+            self._buff = queue.Queue()
+            self.closed = True
+            self.start_time = get_current_time()
+            self.restart_counter = 0
+            self.audio_input = []
+            self.last_audio_input = []
+            self.result_end_time = 0
+            self.is_final_end_time = 0
+            self.final_request_end_time = 0
+            self.bridging_offset = 0
+            self.last_transcript_was_final = False
+            self.new_stream = True
+            self.prevlen = 0
+
+        def sound():
+            while True:
+                data = s.recv(8192)
+                fm.append(data)
+
+                channels = np.frombuffer(data, dtype='int16')
+                c0 = channels[0::8].tobytes()
+                f0.append(c0)
+
+                back = len(f0) - self.prevlen
+                c0 = b''.join(f0[-back:])
+                self._fill_buffer(c0)
+                
+                self.prevlen = len(f0)
+
+        t1 = threading.Thread(target=init) 
+        t2 = threading.Thread(target=sound) 
+
+        t1.start() 
+        t2.start() 
 
     def __enter__(self):
         self.closed = False
         return self
 
     def __exit__(self, type, value, traceback):
-        self._audio_stream.stop_stream()
-        self._audio_stream.close()
         self.closed = True
         self._buff.put(None)
-        self._audio_interface.terminate()
 
     def _fill_buffer(self, in_data, *args, **kwargs):
-        global fm
-        global mic
-
-        # print(len(fm), len(self.audio_input))
-        # self._buff.put(fm[-1])
-
-        # channels = np.frombuffer(fm[-1], dtype='int16')
-        # c0 = channels[0::8].tobytes()
-        mic.append(b''.join(f0[-70:]))
-        # print(len(f0), len(mic), len(self.audio_input))
-        
-        self._buff.put(mic[-1])
-
+        self._buff.put(in_data)
         return None, pyaudio.paContinue
 
     def generator(self):
@@ -139,8 +137,6 @@ class ResumableMicrophoneStream:
                     break
             yield b"".join(data)
 
-finalspeech = ""
-
 def listen_print_loop(responses, stream):
     global finalspeech  
     for response in responses:
@@ -169,8 +165,6 @@ def listen_print_loop(responses, stream):
             - stream.bridging_offset
             + (STREAMING_LIMIT * stream.restart_counter)
         )
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
 
         if result.is_final:
             sys.stdout.write("\033[K")
@@ -201,80 +195,51 @@ def save(name, channels, rate, frames):
     wf.writeframes(b''.join(frames))
     wf.close()
 
-def sound():
-    global s
-    global fm
-    while not exit_signal.is_set():
-        while True:
-            data = s.recv(8192)
-            fm.append(data)
-
-            channels = np.frombuffer(data, dtype='int16')
-            c0 = channels[0::8].tobytes()
-            f0.append(c0)
-
-            # channels = np.frombuffer(fm[-1], dtype='int16')
-            # c0 = channels[0::8].tobytes()
-
 def main():
-    """start bidirectional streaming from microphone input to speech API"""
-    while not exit_signal.is_set():
-        client = speech.SpeechClient()
-        config = speech.RecognitionConfig(
-            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
-            sample_rate_hertz=SAMPLE_RATE,
-            language_code="en-US",
-            max_alternatives=1,
-        )
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=SAMPLE_RATE,
+        language_code="en-US",
+        max_alternatives=1,
+    )
 
-        streaming_config = speech.StreamingRecognitionConfig(
-            config=config, interim_results=True
-        )
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
 
-        mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
-        print("Start Voice Recognition")
+    mic_manager = ResumableMicrophoneStream(SAMPLE_RATE, CHUNK_SIZE)
+    print("Start Voice Recognition")
 
-        with mic_manager as stream:
-            try:
-                while not stream.closed:
-                    stream.audio_input = []
-                    audio_generator = stream.generator()
+    with mic_manager as stream:
+        try:
+            while not stream.closed:
+                stream.audio_input = []
+                audio_generator = stream.generator()
 
-                    requests = (
-                        speech.StreamingRecognizeRequest(audio_content=content)
-                        for content in audio_generator
-                    )
+                requests = (
+                    speech.StreamingRecognizeRequest(audio_content=content)
+                    for content in audio_generator
+                )
 
-                    responses = client.streaming_recognize(streaming_config, requests)
+                responses = client.streaming_recognize(streaming_config, requests)
 
-                    listen_print_loop(responses, stream)
+                listen_print_loop(responses, stream)
 
-                    if stream.result_end_time > 0:
-                        stream.final_request_end_time = stream.is_final_end_time
-                    stream.result_end_time = 0
-                    stream.last_audio_input = []
-                    stream.last_audio_input = stream.audio_input
-                    stream.audio_input = []
-                    stream.restart_counter = stream.restart_counter + 1
-                    if not stream.last_transcript_was_final:
-                        sys.stdout.write("\n")
-                    stream.new_stream = True
-            except KeyboardInterrupt:
-                pass         
+                if stream.result_end_time > 0:
+                    stream.final_request_end_time = stream.is_final_end_time
+                stream.result_end_time = 0
+                stream.last_audio_input = []
+                stream.last_audio_input = stream.audio_input
+                stream.audio_input = []
+                stream.restart_counter = stream.restart_counter + 1
+                if not stream.last_transcript_was_final:
+                    sys.stdout.write("\n")
+                stream.new_stream = True
+        except KeyboardInterrupt:
+            save("speech.wav", 4, 44100, fm)   
+            save("speechchannel.wav", 1, 44100//2, f0)   
+            print("Saved")
+            return         
 
-exit_signal = threading.Event()  # your global exit signal
-
-t1 = threading.Thread(target=main) 
-t2 = threading.Thread(target=sound) 
-
-t1.start() 
-t2.start() 
-
-try:
-    while not exit_signal.is_set(): 
-        time.sleep(0.1)  
-except KeyboardInterrupt:  
-    exit_signal.set()  
-    save("speech.wav", 4, 44100, fm)   
-    save("mic.wav", 1, 44100//2, mic)   
-    print("Saved")
+main()
