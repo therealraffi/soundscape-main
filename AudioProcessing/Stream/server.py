bac1b6f4f54064978778a5f85d2145f66491b7ce
Raffu Khondaker <2022rkhondak@tjhsst.edu>
2020-12-29 13:55:06 -0500

    stuff

--- AudioProcessing/Stream/server.py
+++ AudioProcessing/Stream/server.py
@@ -8,22 +8,23 @@ ip = socket.gethostbyname(socket.gethostname())
 
 mic = pyaudio.PyAudio()
 
-FORMAT = pyaudio.paInt16
+FORMAT = pyaudio.paFloat32
 CHANNELS = 1
 RATE = 44100
 CHUNK = 8192
+RECORD_SECONDS = 1.5
 stream = mic.open(format=FORMAT, channels=CHANNELS, rate=RATE,
                   input_device_index=2,
                   input=True, frames_per_buffer=CHUNK)
 
-global fm 
+global fm
 fm = []
 
 while True:
     try:
         port = 5500
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind((ip, port))
+        s.bind(('', port))
         break
     except Exception as e:
         print(e)
@@ -47,9 +48,8 @@ def accept_connections():
         try:
             c, addr = s.accept()
             connections.append(c)
-            threading.Thread(target=handle_client,args=(c,addr,)).start()
+            threading.Thread(target=handle_client,args=(c,addr)).start()
         except KeyboardInterrupt as e:
-            save("server-combined.wav", 1, RATE, fm)
             exit()
 
 def broadcast(sock, data):
@@ -64,12 +64,21 @@ def handle_client(c, addr):
     print("Start")
     while True:
         try:
-            data = stream.read(CHUNK, exception_on_overflow=False)
+            frames = np.array([])
+            data = 0
+            for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+                data = stream.read(CHUNK, exception_on_overflow=False)
+                frames = np.append(frames, np.fromstring(data, dtype=np.float32))
+            print(len(frames))
+            # data = stream.read(CHUNK, exception_on_overflow=False)
             fm.append(data)
-            broadcast(c, data)
+            broadcast(c, frames.tobytes())
 
         except socket.error:
+            save("server-combined.wav", 1, RATE, fm)
+            print('Saved')
             c.close()
+            exit()
 
 connections = []
 accept_connections()
