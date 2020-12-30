994b490fedcaa58516e9bc3c2b633a48507206de
inafi <irfan.k.nafi@gmail.com>
2020-12-29 21:53:57 -0500

    2 arduinos

--- AudioProcessing/Stream/main.py
+++ AudioProcessing/Stream/main.py
@@ -304,8 +304,6 @@ def avgfreq(block):
 
         return None if math.isnan(float(out)) or len(freqdict) == 0 else max(1, int(out))
 
-#print("%3s %6s \t %3s %6s \t %3s %6s \t %3s %6s" % (amplitude(f0[-1]), avgfreq(f0[-1]), amplitude(f1[-1]), avgfreq(f1[-1]), amplitude(f2[-1]), avgfreq(f2[-1]), amplitude(f3[-1]), avgfreq(f3[-1])), amp * "|")
-
 def getanalysis():
 
     ip = '192.168.1.218'
@@ -360,13 +358,14 @@ def arduino():
     global angles
     global analysis
 
-    s = serial.Serial(port='/dev/tty.usbserial-14230', baudrate=115200)
+    ardhigh = serial.Serial(port='/dev/tty.usbmodem142301', baudrate=115200)
+    ardlow = serial.Serial(port='/dev/tty.usbserial-141110', baudrate=115200)
 
     sound = db.reference('Sound')   
 
     while True:
         try:
-            channels = np.frombuffer(postdata, dtype='int16')
+            channels = np.frombuffer(sepdata, dtype='int16')
             c0 = channels[0::8].tobytes() #red
             c1 = channels[1::8].tobytes() #green
             c2 = channels[2::8].tobytes() #blue
@@ -381,7 +380,7 @@ def arduino():
                 if (180 - ignore)/2 < i[0] < (180 + ignore)/2:
                     del angles[c]
 
-            amps = [0] * 6
+            motors = [0] * 6
             inc = (360 - ignore) / 5
 
             possible = [(180 + ignore)/2 + i * inc for i in range(5)]
@@ -392,32 +391,49 @@ def arduino():
                 for c, k in enumerate(possible):
                     if k - inc/2 <= angle <= k + inc/2:
                         ind = c
-                        if amps[ind] != 0:
+                        if motors[ind] != 0:
                             if ind == 5:
-                                if amps[0] == 0:
+                                if motors[0] == 0:
                                     ind = 0
                                     break
-                                if amps[4] == 0:
+                                if motors[4] == 0:
                                     ind = 4
                                     break
                             else:
-                                if amps[ind + 1] == 0:
+                                if motors[ind + 1] == 0:
                                     ind += 1
                                     break
-                                if amps[ind - 1] == 0:
+                                if motors[ind - 1] == 0:
                                     ind -= 1
                                     break
                         else:
                             break
-                amps[ind] = max(analysis[channel][0] * 80/100, 0) if analysis[channel][0] > 3 else 0
+                motors[ind] = [max(analysis[channel][0] * 80/100, 0), analysis[channel][1]] if analysis[channel][0] > 3 else 0
 
             # print(angles)
             # print(analysis)
-            # print(amps)
+            # print(motors)
             # print("\n\n")
 
-            out = "<%s, %s, %s, %s, %s, %s>" % (amps[0], amps[1], amps[2], amps[3], amps[4], amps[5])
-            s.write(out.encode())
+            print("%3s %6s \t %3s %6s \t %3s %6s \t %3s %6s" % (analysis[0][0], analysis[0][1], analysis[1][0], analysis[1][1], analysis[2][0], analysis[2][0], analysis[3][0], analysis[3][1]), amplitude(sepdata) * "|")
+
+            low = [0] * 6
+            high = [0] * 6
+
+            for c, i in enumerate(motors):
+                if i == 0:
+                    continue
+                elif i[1] > 640:
+                    high[c] = i[0]
+                else:
+                    low[c] = i[0]
+
+            out = "<%s, %s, %s, %s, %s, %s>" % (low[0], low[1], low[2], low[3], low[4], low[5])
+            ardlow.write(out.encode())
+            
+            out = "<%s, %s, %s, %s, %s, %s>" % (high[0], high[1], high[2], high[3], high[4], high[5])
+            ardhigh.write(out.encode())
+            
         except Exception as e:
             pass
 
@@ -553,28 +569,14 @@ def listen_print_loop(responses, stream, channelnum):
         ref.child("sound" + str(channelnum + 1)).child("content").set(transcript)
 
         if result.is_final:
-            #where speech is finalized
             print(channelnum, transcript)
-
-            #Temp sys.stdout.write("\033[K")
-            
-            # sys.stdout.write(str(corrected_time) + ": " + transcript + "\n")
-
-            #Temp sys.stdout.write(transcript + "\n")
-
             stream.is_final_end_time = stream.result_end_time
             stream.last_transcript_was_final = True
 
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
             if re.search(r"\b(exit|quit)\b", transcript, re.I):
-                #Temp sys.stdout.write("Exiting...\n")
                 stream.closed = True
                 break
         else:
-            #Temp sys.stdout.write("\033[K")
-            # sys.stdout.write(str(corrected_time) + ": " + transcript + "\r")
-            #Temp sys.stdout.write(transcript + "\r")
             stream.last_transcript_was_final = False
 
 def main(channelnum):
@@ -629,10 +631,10 @@ if __name__ == "__main__":
     t5 = threading.Thread(target=arduino) 
     t6 = threading.Thread(target=getanalysis) 
 
-    s1 = threading.Thread(target=main, kwargs={'channelnum': 0})
-    s2 = threading.Thread(target=main, kwargs={'channelnum': 1})
-    s3 = threading.Thread(target=main, kwargs={'channelnum': 2})
-    s4 = threading.Thread(target=main, kwargs={'channelnum': 3})
+    # s1 = threading.Thread(target=main, kwargs={'channelnum': 0})
+    # s2 = threading.Thread(target=main, kwargs={'channelnum': 1})
+    # s3 = threading.Thread(target=main, kwargs={'channelnum': 2})
+    # s4 = threading.Thread(target=main, kwargs={'channelnum': 3})
 
     try:
         t1.start() 
@@ -642,10 +644,10 @@ if __name__ == "__main__":
         t5.start() 
         t6.start() 
 
-        s1.start() 
-        s2.start() 
-        s3.start() 
-        s4.start() 
+        # s1.start() 
+        # s2.start() 
+        # s3.start() 
+        # s4.start() 
 
         t1.join() 
         t2.join() 
@@ -654,10 +656,10 @@ if __name__ == "__main__":
         t5.join() 
         t6.join() 
 
-        s1.join() 
-        s2.join() 
-        s3.join() 
-        s4.join() 
+        # s1.join() 
+        # s2.join() 
+        # s3.join() 
+        # s4.join() 
     except:
         print("\n\n\n\n\n\n\n\nEnd")
   
